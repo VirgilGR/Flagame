093f2c5978b46a5e1a7e4d39e46e717188cf3041
ERWANN GUEGAN (Etudiant) <guegaerw@ur.univ-rouen.fr>
2022-10-18 15:57:18 +0200

    Création visuel des questions

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/js', express.static(__dirname + '/node_modules/@popperjs/core/dist/umd'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/css', express.static(__dirname + '/www/css/'));
+
+
 
 
 
